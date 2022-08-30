30cbc5c977481747a10ec6617994a90930c85e69
Sukhdev Gill <26435947+sukhdevgill@users.noreply.github.com>
2022-08-30 10:25:42 +0100

    Add Phase 1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,42 @@
 import Board from "./board.js";
 
-let board = new Board(); // creates a new game board
+document.addEventListener("DOMContentLoaded", () => {
+  // Examine the grid of the game board in the browser console
+  // Create the UI of the game using HTML elements based on this grid
+  let board = new Board();
+  console.log(board.grid);
 
-// Examine the grid of the game board in the browser console
-// Create the UI of the game using HTML elements based on this grid
-console.log(board.grid);
+  const createContainer = (className) => {
+    const container = document.createElement("div");
+    container.className = className;
+    document.body.appendChild(container);
+  }
+
+  const createGrid = (rows, cols) => {
+    for (let i = 1; i <= rows; i++) {
+      for (let j = 1; j <= cols; j++) {
+        const square = document.createElement("div");
+        square.className = "square";
+        square.setAttribute("data-row", i);
+        square.setAttribute("data-col", j);
+        document.querySelector(".container").appendChild(square);
+      }
+    }
+  }
+
+  createContainer('container');
+  createGrid(board.numRows, board.numCols);
+
+});
+
+
+// Grid:
+// [null,    3, null, null, null, null, null, null, null]
+// [null,    3, null, null, null, null, null, null, null]
+// [null,    3,    3, null, null, null, null, null, null]
+// [null, null,    3, null,    5,    5,    5,    5,    5]
+// [null, null,    3, null, null, null, null, null, null]
+// [null,    4, null, null, null, null, null, null, null]
+// [null,    4, null, null, null, null, null, null, null]
+// [null,    4, null, null, null, null, null, null,    2]
+// [null,    4, null, null, null, null, null, null,    2]
